787383af555c3eead7327864e0dbadb9c2272afd
Jonas Savickas <jonsavickas@gmail.com>
2025-03-17 21:48:05 +0200

    first

---
+++ sprint-1/1packages-build-tools/js-project/.eslintrc.cjs
@@ -0,0 +1,28 @@
+// Here we are using CommonJS modules instead of ES modules.
+// That is also the reason why we are using .cjs file extension.
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-param-reassign': 'off',
+  },
+};

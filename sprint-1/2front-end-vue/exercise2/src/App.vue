1d254acb86f4d962653374734bf98af52d62b222
Jonas Savickas <jonsavickas@gmail.com>
2025-03-24 20:44:01 +0200

    push

---
+++ sprint-1/2front-end-vue/exercise2/src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <header>
+    <h1>Hello World!</h1>
+  </header>
+
+  <main>
+    <!-- content here -->
+  </main>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+</style>
